c55034029b28d10b7e4e69e848a80e7f9ecf6e57
MMDREZA <mheidari831003.game@gmail.com>
2023-07-12 11:43:12 +0330

    added mmd.py

---
+++ mmd.py
@@ -0,0 +1,22 @@
+name = input('please give me your name: ')
+a = int(input ('enter age:'))
+h = int(input('entet your height'))
+
+
+def jam(a,b) :
+    javab = a + b
+    return javab     
+
+def say_hello(name):
+        print (name)
+        print ('hi')
+        print ('who are you?')
+
+if name == 'mmd':
+         say_hello('mmd')
+
+         print('seneh shoma')
+         print (jam (a,h))
+else :
+    print('hi ',name)
+
